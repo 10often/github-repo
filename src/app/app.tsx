be71bd28fa80951e7c216bab77a9438d42f3482c
10often <vl10often@gmail.com>
2023-07-31 20:14:01 +0600

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="github-repo" />
+    </div>
+  );
+}
+
+export default App;
